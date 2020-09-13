d2fd64ae30f948879d78acc529b2f9b7d79ee5c3
cghtompkins17 <71199219+cghtompkins17@users.noreply.github.com>
2020-09-13 08:24:30 +0530

    Add files via upload

---
+++ IdeaProjects/src/com/chess/engine/pieces/Bishop.java
@@ -0,0 +1,81 @@
+package com.chess.engine.pieces;
+
+import com.chess.engine.Alliance;
+import com.chess.engine.board.Board;
+import com.chess.engine.board.BoardUtils;
+import com.chess.engine.board.Move;
+import com.chess.engine.board.Move.AttackMove;
+import com.chess.engine.board.Move.MajorMove;
+import com.chess.engine.board.Tile;
+import com.google.common.collect.ImmutableList;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import static com.chess.engine.pieces.Piece.PieceType.BISHOP;
+
+
+public class Bishop extends Piece {
+
+    private final static int[] CANDIDATE_MOVE_VECTOR_COORDINATES = {-9,-7,7,9};
+    public Bishop( Alliance pieceAlliance, int piecePosition) {
+        super(BISHOP, piecePosition, pieceAlliance);
+    }
+
+    @Override
+    public Collection<Move> calculateLegalMoves(Board board) {
+
+        final List<Move> legalMoves = new ArrayList<>();
+        for(final int candidateCoordinateOffset : CANDIDATE_MOVE_VECTOR_COORDINATES){
+            int candidateDestinationCoordinate = this.piecePosition;
+
+            while(BoardUtils.isValidTileCoordinate(candidateDestinationCoordinate)){
+
+                if(isFirstColumn(candidateDestinationCoordinate, candidateCoordinateOffset) ||
+                isEightColumn(candidateDestinationCoordinate, candidateCoordinateOffset)){
+                    break;
+                }
+                candidateDestinationCoordinate += candidateCoordinateOffset;
+                if(BoardUtils.isValidTileCoordinate(candidateDestinationCoordinate)){
+                    final Tile candidateDestinationTile = board.getTile(candidateDestinationCoordinate);
+                    if(!candidateDestinationTile.isTileOccupied()){
+                        legalMoves.add(new MajorMove(board, this, candidateDestinationCoordinate));
+                    }
+                    else{
+                        final Piece pieceAtDestination = candidateDestinationTile.getPiece();
+                        final Alliance pieceAlliance = pieceAtDestination.getPieceAlliance();
+                        if(this.pieceAlliance != pieceAlliance){
+                            legalMoves.add(new AttackMove(board, this, candidateDestinationCoordinate, pieceAtDestination));
+                        }
+                        break;
+                    }
+
+                }
+            }
+        }
+        System.out.println((legalMoves.isEmpty()?"No bishop move":"Bishop has moves"));
+        return ImmutableList.copyOf(legalMoves);
+    }
+
+    @Override
+    public Bishop movePiece(final Move move) {
+        return new Bishop(move.getMovedPiece().getPieceAlliance(), move.getDestinationCoordinate());
+    }
+
+    @Override
+    public String toString() {
+        return BISHOP.toString();
+    }
+
+    private static boolean isFirstColumn(final int currentPosition, final int candidateOffset){
+        return BoardUtils.FIRST_COLUMN[currentPosition] && (candidateOffset == -9 || candidateOffset == 7);
+    }
+
+    private static boolean isEightColumn(final int currentPosition, final int candidateOffset){
+        return BoardUtils.EIGHT_COLUMN[currentPosition] && (candidateOffset == 9 || candidateOffset == -7);
+    }
+
+
+
+}
